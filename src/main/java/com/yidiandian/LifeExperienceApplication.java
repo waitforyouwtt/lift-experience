f25f56ed596ebf7c8b519f3904b619e93b91ee0c
luoxian <15290810931@163.com>
2019-03-09 14:46:19 +0800

    第一版：创建项目

---
+++ src/main/java/com/yidiandian/LifeExperienceApplication.java
@@ -0,0 +1,13 @@
+package com.yidiandian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeExperienceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LifeExperienceApplication.class, args);
+    }
+
+}
